da948be0d1c6a483102a22c8a0be65a3f95d2300
Michael Markieta <markieta.m@gmail.com>
2020-12-03 00:56:57 -0500

    consolidate i32 parsing in the expense_report Vec creation

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,30 @@ use std::env;
 use std::collections::HashMap;
 use reqwest;
 
-fn d01p01(expense_report: &Vec<&str>) {
+fn d01p01(expense_report: &Vec<i32>) {
     let answer1: i32;
     let mut expense_report_hashmap: HashMap<i32, i32> = HashMap::new();
     for itm in expense_report.iter() {
-        let itm_int = itm.parse::<i32>().unwrap();
-        let itm_remainder = 2020 - itm_int;
+        let itm_remainder = 2020 - itm;
         if !expense_report_hashmap.contains_key(&itm_remainder) {
-            expense_report_hashmap.insert(itm_int, itm_remainder);
+            expense_report_hashmap.insert(*itm, itm_remainder);
         } else {
-            println!("We found a pair of values that add up to 2020: {} and {}", itm_int, itm_remainder);
-            answer1 = itm_int * itm_remainder;
+            println!("We found a pair of values that add up to 2020: {} and {}", itm, itm_remainder);
+            answer1 = itm * itm_remainder;
             println!("Multiply them together and get: {}", answer1);
             return
         }
     }
 }
 
-fn d01p02(expense_report: &Vec<&str>) {
+fn d01p02(expense_report: &Vec<i32>) {
     let answer2: i32;
     for itm_a in expense_report.iter() {
-        let itm_a_int = itm_a.parse::<i32>().unwrap();
         for itm_b in expense_report.iter() {
-            let itm_b_int = itm_b.parse::<i32>().unwrap();
             for itm_c in expense_report.iter() {
-                let itm_c_int = itm_c.parse::<i32>().unwrap();
-                if itm_a_int + itm_b_int + itm_c_int == 2020 {
-                    println!("We found three values that add up to 2020: {}, {} and {}", itm_a_int, itm_b_int, itm_c_int);
-                    answer2 = itm_a_int * itm_b_int * itm_c_int;
+                if itm_a + itm_b + itm_c == 2020 {
+                    println!("We found three values that add up to 2020: {}, {} and {}", itm_a, itm_b, itm_c);
+                    answer2 = itm_a * itm_b * itm_c;
                     println!("Multiply them together and get: {}", answer2);
                     return
                 }
@@ -50,7 +46,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .text()
         .unwrap();
 
-    let expense_report = response.trim_end().split("\n").collect::<Vec<&str>>();
+    let expense_report = response.trim_end().split("\n").map(|x| x.parse::<i32>().unwrap()).collect();
 
     d01p01(&expense_report);
     d01p02(&expense_report);
