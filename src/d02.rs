25c52ce0cfc5aa18a24ab750fb1fcf5c9cec756e
Michael Markieta <markieta.m@gmail.com>
2020-12-05 22:28:43 -0500

    dont use hashmaps for password dumdum

--- src/d02.rs
+++ src/d02.rs
@@ -1,20 +1,50 @@
-use std::collections::HashMap;
-
-fn d02p01(passwords: &HashMap<String, String>) {
-    for (policy, password) in passwords {
-        let policy_parts: Vec<&str> = policy.split_whitespace().collect();
-        let policy_lengths: Vec<&str> = policy_parts[0].split("-").collect();
-        let policy_lengths_min: i32 = policy_lengths[0].parse().unwrap();
-        let policy_lengths_max: i32 = policy_lengths[1].parse().unwrap();
-        let policy_char = policy_parts[1];
-        println!("Min: {}, Max: {}, Char: {}, Password: {}", policy_lengths_min, policy_lengths_max, policy_char, password);
-    }
+#[derive(Debug)]
+struct Policy {
+    password: String,
+    min: u16,
+    max: u16,
+    char: String,
 }
 
-pub fn d02(data: Vec<String>) {
-    let passwords: HashMap<String, String> = data.into_iter()
-        .map(|s| s.split(":").map(|s| s.to_string()).collect::<Vec<String>>())
-        .map(|vec| (vec[0].to_string(), vec[1].trim_start().to_string()))
-        .collect();
+fn d02p01(policies: &Vec<Policy>) {
+    let mut good_passwords = 0;
+    let mut _bad_passwords = 0;
+    for policy in policies {
+        let min = policy.min;
+        let max = policy.max;
+        let count = policy.password.matches(&policy.char).count() as u16;
+        if count >= min && count <= max {
+            good_passwords += 1;
+            // println!("{}, {}, {}, {}, {}, {}", &policy.char, min, max, count, password, true);
+        } else {
+            _bad_passwords += 1;
+            // println!("{}, {}, {}, {}, {}, {}", &policy.char, min, max, count, password, false);
+        }
+    }
+    println!("Answer to d02p01: {}", good_passwords);
+    println!("{}, {}", good_passwords, _bad_passwords);
+}
+
+pub fn d02(data: Vec<String>) {  
+    let mut passwords: Vec<Policy> = Vec::new(); // [{password:p,min:x,max:y,char:z,}]
+
+    for line in data {
+        let line_split = line.split(":").map(|s| s.trim_start().trim_end().to_string()).collect::<Vec<String>>();
+        let policy = &line_split[0];
+        let password = &line_split[1];
+        let policy_parts: Vec<&str> = policy.split_whitespace().collect();
+        let policy_lengths: Vec<u16> = policy_parts[0].split("-").map(|s| s.parse().unwrap()).collect::<Vec<u16>>();
+        let policy_lengths_min: u16 = policy_lengths[0];
+        let policy_lengths_max: u16 = policy_lengths[1];
+        let policy_char = policy_parts[1];
+        
+        passwords.push(Policy{
+            password: password.to_string(),
+            min: policy_lengths_min,
+            max: policy_lengths_max,
+            char: policy_char.to_string(),
+        });
+    }
+
     d02p01(&passwords);
 }
