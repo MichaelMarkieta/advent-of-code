e865d32988a91d73b9837a616aa972284e910974
Michael Markieta <markieta.m@gmail.com>
2020-12-06 15:16:40 -0500

    completed d03p01

--- src/d03.rs
+++ src/d03.rs
@@ -1,12 +1,29 @@
+fn d03p01(map: &Vec<String>){
+    let mut encountered: Vec<String> = Vec::new();
+    let mut x: i32 = 0;
+    for row in map {
+        match row.chars().nth(x as usize) {
+            Some(v) => encountered.push(v.to_string()),
+            None => continue,
+        }
+        x += 3;
+    }
+    let count_trees_encountered = encountered.iter().filter(|&s| *s == "#").count();
+    println!("Answer to d03p01: {}", count_trees_encountered);
+}
+
+
 pub fn d03(data: Vec<String>) {
-    let num_lines = data.len();
-    let line_length_required = (num_lines / 3) as f32; // we move across 3 for every line in the input
     let line_length = data[0].len() as f32;
-    let line_length_factor = ((line_length_required / line_length) as f32).ceil() as usize; // extend the lines by this factor to have enough data
+    let line_length_required = (line_length / 3.0) as f32;
+    let num_lines = data.len() as f32;
+    let line_length_factor = ((num_lines / line_length_required) as f32).ceil() as usize;
     
     let mut map: Vec<String> = Vec::new();
     for line in data {
         let repeated_line = line.repeat(line_length_factor);
         map.push(repeated_line);
     }
+
+    d03p01(&map);
 }
